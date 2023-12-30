fd5b759f2c8b7d629c943689d3b383f1735f328a
AnkitGhildiyal2001 <ce19b005@iittp.ac.in>
2023-12-30 19:18:08 +0530

    gh pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ProfileDashboard from './pages/ProfileDashboard';
 import PricingPage from './pages/PricingPage';
 import GraphDetailsPage from './pages/GraphDetailsPage';
